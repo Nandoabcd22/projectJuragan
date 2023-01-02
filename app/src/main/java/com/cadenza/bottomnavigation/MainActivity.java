af2fda9056f058c158e7a6b4374ddce6f68a7829
shiran02 <shirankumarasingha97@gmail.com>
2023-01-02 10:03:31 +0530

    initial commit

---
+++ app/src/main/java/com/cadenza/bottomnavigation/MainActivity.java
@@ -0,0 +1,63 @@
+package com.cadenza.bottomnavigation;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.WindowManager;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView navigationView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //hide line status bar
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        navigationView = findViewById(R.id.bottom_navigation);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.body_container , new HomeFragment()).commit();
+        navigationView.setSelectedItemId(R.id.nav_home);
+
+
+        navigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                Fragment fragment = null;
+
+                switch (item.getItemId()){
+
+                    case R.id.nav_home:
+                        fragment = new HomeFragment();
+                        break;
+                    case R.id.nav_search:
+                        fragment = new SearchFragment();
+                        break;
+
+                    case R.id.nav_shop:
+                        fragment = new SearchFragment();
+                        break;
+                    case R.id.nav_like:
+                        fragment = new LikeFragment();
+                        break;
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.body_container,fragment).commit();
+
+
+                return true;
+            }
+        });
+    }
+}
